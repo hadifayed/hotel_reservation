116a4af39ee0dc0674d356965655193aee0cbef9
Hadi <hagfayed@gmail.com>
2023-10-11 15:14:09 +0300

    add rooms actions

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,37 @@
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+  
+  # GET /rooms
+  def index
+    @rooms = Room.all
+
+    render json: @rooms
+  end
+
+  # POST /rooms
+  def create
+    @room = Room.new(room_params)
+
+    if @room.save
+      render json: @room, status: :created, location: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /rooms/1
+  def update
+    @room = Room.find(params[:id])
+    if @room.update(room_params)
+      render json: @room
+    else
+      render json: @room.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+    def room_params
+      params.require(:room).permit(:description, :capacity, :price_per_night)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :rooms, only: [:create, :update, :index]
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:sessions, :password, :registration]
   devise_scope :user do
     resource :session, only: [], path: 'auth', controller: 'sessions', as: :user_session do
