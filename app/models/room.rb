2fd16bcfaa0030ec2cd9e75148dcf254afbd5892
Hadi <hagfayed@gmail.com>
2023-10-14 12:03:39 +0300

    fix wrong indentation and add a comment

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,10 +1,11 @@
 class Room < ApplicationRecord
-	has_many :room_reservations, dependent: :destroy
+  has_many :room_reservations, dependent: :destroy
 
-	validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-	validates :price_per_night, presence: true, numericality: { greater_than: 0 }
+  validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price_per_night, presence: true, numericality: { greater_than: 0 }
 
-	def locking_enabled?
-		super && persisted? && reservation_count_changed?
-	end
+  # overriding the originla method to enable locking only when th reservation_count field is changed 
+  def locking_enabled?
+    super && persisted? && reservation_count_changed?
+  end
 end
