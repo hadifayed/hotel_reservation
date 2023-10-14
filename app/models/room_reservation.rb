2f14da134b9c19ffcc8c52c793e51af461cdcc76
Hadi <hagfayed@gmail.com>
2023-10-14 12:02:54 +0300

    run check_in and check_out validations only if they are provided or changed

--- app/models/room_reservation.rb
+++ app/models/room_reservation.rb
@@ -13,32 +13,33 @@ class RoomReservation < ApplicationRecord
 
   private
 
+  def should_not_validate_check_in_and_check_out?
+    return true if check_in.blank? || check_out.blank? || !check_in_changed? || !check_out_changed?
+
+    false
+  end
+
   def check_in_and_check_out_are_not_in_the_past
-    return if check_in.blank? || check_out.blank?
+    return if should_not_validate_check_in_and_check_out?
 
     errors.add(:check_out, :past_error) if Date.current > check_out
     errors.add(:check_in, :past_error) if Date.current > check_in
   end
 
   def check_out_is_after_check_in
-    return if check_in.blank? || check_out.blank?
+    return if should_not_validate_check_in_and_check_out?
 
-    if check_out < check_in
-      errors.add(:check_out, :after_check_in)
-    end
+    errors.add(:check_out, :after_check_in) if check_out < check_in
   end
 
   def room_is_available
-    return if check_in.blank? || check_out.blank?
+    return if should_not_validate_check_in_and_check_out?
 
     RoomReservation.pending.where(room_id: room_id).each do |reservation|
-      next if reservation.id == id
-
       check_in_within_reserved_period = (reservation.check_in..reservation.check_out) === check_in
       check_out_within_reserved_period = (reservation.check_in..reservation.check_out) === check_out
-      if check_in_within_reserved_period || check_out_within_reserved_period
-        errors.add(:room, :not_available)
-      end
+
+      errors.add(:room, :not_available) if check_in_within_reserved_period || check_out_within_reserved_period
     end
   end
 end
