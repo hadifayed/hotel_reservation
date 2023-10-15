144c04cf1f0529cab2d109927f4ca7d5fa00b28c
Hadi <hagfayed@gmail.com>
2023-10-16 00:06:02 +0300

    add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_130807) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_15_205619) do
   create_table "room_reservations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_130807) do
     t.text "tokens"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 0, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'Enums' do
+    it { should define_enum_for(:role).with_values([:guest, :admin]) }
+  end
+
   describe 'Associations' do
     it { is_expected.to have_many(:room_reservations) }
   end
